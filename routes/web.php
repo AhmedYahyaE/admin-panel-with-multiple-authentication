365771f2fff03012abc7c6b75d533f940657f9bf
Ahmed Yahya <a.yahya1@yahoo.com>
2024-12-20 19:50:08 +0200

    chore: Create initial Views

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,34 @@
 <?php
 
+// use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{DashboardController, UserController, ProjectController};
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+// Start Laravel Breeze Routes
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+    // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+require __DIR__.'/auth.php';
+// End Laravel Breeze Routes
+
+
+
+// My Routes
+Route::middleware('auth')->group(function () {
+    Route::get('/', function() {
+        return redirect()->route('dashboard');
+    });
+    Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
+
+    Route::get('/users'   , [UserController::class, 'index'])->name('users');
+    Route::get('/projects', [ProjectController::class, 'index'])->name('projects');
 });
